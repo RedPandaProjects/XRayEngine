48a29cd22da0e24034a2bc6b94e70691e0a1d7ad
BearIvan <i-sobolevskiy@mail.ru>
2020-06-02 16:06:15 +0700

    * ADD:Shader Editor

---
+++ Source/Editors/ShaderEditor/ShaderEditor.cpp
@@ -0,0 +1,22 @@
+// ShaderEditor.cpp : Определяет точку входа для приложения.
+//
+#include "stdafx.h"
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    Core._initialize("Shader", ELogCallback, 1, "fs.ltx", true);
+
+    Tools = xr_new<CShaderTool>();
+    STools = (CShaderTool*)Tools;
+    UI = xr_new<CShaderMain>();
+    UI->RegisterCommands();
+
+    UIMainForm* MainForm = xr_new< UIMainForm>();
+    ::MainForm = MainForm;
+    UI->Push(MainForm, false);
+    while (MainForm->Frame())
+    {
+    }
+    xr_delete(MainForm);
+    Core._destroy();
+    return 0;
+}
---
+++ Source/Editors/ShaderEditor/stdafx.h
@@ -0,0 +1,33 @@
+//----------------------------------------------------
+// file: stdafx.h
+//----------------------------------------------------
+#pragma once
+#include "..\XrECore\stdafx.h"
+#include "..\XrECore\Editor\device.h"
+#include "..\XrECore\Editor\UI_MainCommand.h"
+#include "..\XrECore\Editor\UI_ToolsCustom.h"
+#include "..\XrECore\Editor\ui_main.h"
+#include "..\XrECore\Editor\EditorPreferences.h"
+#include "..\XrECore\Editor\render.h"
+#include "..\XrECore\Editor\UIRenderForm.h"
+#include "..\XrECore\Editor\ELog.h"
+#include "..\XrECore\Editor\Library.h"
+
+#include "UI_shadertools.h"
+#include "ui_shadermain.h"
+#include "UIMainMenuForm.h"
+#include "UILeftBarForm.h"
+#include "UITopBarForm.h"
+#include "UIMainForm.h"
+
+inline void not_implemented_low()
+{
+}
+inline void not_implemented()
+{
+	not_implemented_low();
+	return;
+	if (IsDebuggerPresent())
+		DebugBreak();
+	R_ASSERT(0);
+}
