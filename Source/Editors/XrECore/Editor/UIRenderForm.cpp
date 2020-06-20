e2bcb8a84d1d440f5da1b82ee767358820236287
BearIvan <i-sobolevskiy@mail.ru>
2020-06-20 12:53:13 +0700

    * FIX:[XrECore] Box selection arbitrarily start when form without focused

--- Source/Editors/XrECore/Editor/UIRenderForm.cpp
+++ Source/Editors/XrECore/Editor/UIRenderForm.cpp
@@ -31,46 +31,62 @@ void UIRenderForm::Draw()
 		ImDrawList* draw_list = ImGui::GetWindowDrawList();
 		ImVec2 canvas_pos = ImGui::GetCursorScreenPos();
 		ImVec2 canvas_size = ImGui::GetContentRegionAvail();
-		if (canvas_size.x < 64)canvas_size.x = 64;
-		if (canvas_size.y < 64)canvas_size.y = 64;
-		if ((ImGui::GetIO().MousePos.x >= canvas_pos.x && ImGui::GetIO().MousePos.y >= canvas_pos.y) &&
-			(ImGui::GetIO().MousePos.x <= (canvas_pos.x + canvas_size.x) && ImGui::GetIO().MousePos.y <= (canvas_pos.y + canvas_size.y)))
+		ImVec2 mouse_pos = ImGui::GetIO().MousePos;
+		if (mouse_pos.x < canvas_pos.x)
 		{
-			if (ImGui::IsWindowFocused())
-			{
-
-				if ((ImGui::IsMouseDown(ImGuiMouseButton_Left) || ImGui::IsMouseDown(ImGuiMouseButton_Right)) && !m_mouse_down)
-				{
-					UI->MousePress(TShiftState(ShiftState), ImGui::GetIO().MousePos.x - canvas_pos.x, ImGui::GetIO().MousePos.y - canvas_pos.y);
-					m_mouse_down = true;
-				}
-
-				else  if (ImGui::IsMouseReleased(ImGuiMouseButton_Left) || ImGui::IsMouseReleased(ImGuiMouseButton_Right))
-				{
-					UI->MouseRelease(TShiftState(ShiftState), ImGui::GetIO().MousePos.x - canvas_pos.x, ImGui::GetIO().MousePos.y - canvas_pos.y);
-					m_mouse_down = false;
-					m_mouse_move = false;
-				}
-				else
-				{
-					UI->MouseMove(TShiftState(ShiftState), ImGui::GetIO().MousePos.x - canvas_pos.x, ImGui::GetIO().MousePos.y - canvas_pos.y);
-					m_mouse_move = true;
-				}
-			}
-				
-			m_mouse_position.set(ImGui::GetIO().MousePos.x - canvas_pos.x, ImGui::GetIO().MousePos.y - canvas_pos.y);
+			mouse_pos.x = canvas_pos.x;
 		}
-		else  if (ImGui::IsMouseReleased(ImGuiMouseButton_Left) || ImGui::IsMouseReleased(ImGuiMouseButton_Right))
+		if (mouse_pos.y < canvas_pos.y)
 		{
-			UI->MouseRelease(TShiftState(ShiftState), ImGui::GetIO().MousePos.x - canvas_pos.x, ImGui::GetIO().MousePos.y - canvas_pos.y);
+			mouse_pos.y = canvas_pos.y;
+		}
+
+		if (mouse_pos.x > canvas_pos.x + canvas_size.x)
+		{
+			mouse_pos.x = canvas_pos.x + canvas_size.x;
+		}
+		if (mouse_pos.y > canvas_pos.y + canvas_size.y)
+		{
+			mouse_pos.y = canvas_pos.y + canvas_size.y;
+		}
+
+
+		if (ImGui::IsWindowFocused())
+		{
+
+			if ((ImGui::IsMouseDown(ImGuiMouseButton_Left) || ImGui::IsMouseDown(ImGuiMouseButton_Right)) && !m_mouse_down)
+			{
+				UI->MousePress(TShiftState(ShiftState), mouse_pos.x - canvas_pos.x, mouse_pos.y - canvas_pos.y);
+				m_mouse_down = true;
+			}
+
+			else  if (ImGui::IsMouseReleased(ImGuiMouseButton_Left) || ImGui::IsMouseReleased(ImGuiMouseButton_Right))
+			{
+				UI->MouseRelease(TShiftState(ShiftState), mouse_pos.x - canvas_pos.x, mouse_pos.y - canvas_pos.y);
+				m_mouse_down = false;
+				m_mouse_move = false;
+			}
+			else if (m_mouse_down)
+			{
+				UI->MouseMove(TShiftState(ShiftState), mouse_pos.x - canvas_pos.x, mouse_pos.y - canvas_pos.y);
+				m_mouse_move = true;
+			}
+		}
+		else  if (m_mouse_down)
+		{
+			UI->MouseRelease(TShiftState(ShiftState), mouse_pos.x - canvas_pos.x, mouse_pos.y - canvas_pos.y);
 			m_mouse_down = false;
 		}
-		
+
+		m_mouse_position.set(mouse_pos.x - canvas_pos.x, mouse_pos.y - canvas_pos.y);
+
+
+
 		UI->RTSize.set(canvas_size.x, canvas_size.y);
 		if (canvas_size.x < 50.0f) canvas_size.x = 50.0f;
 		if (canvas_size.y < 50.0f) canvas_size.y = 50.0f;
 		ImGui::InvisibleButton("canvas", canvas_size);
-		if (!m_OnContextMenu.empty()&& !m_mouse_move&& !(ShiftState& ssShift))
+		if (!m_OnContextMenu.empty() && !m_mouse_move && !(ShiftState & ssShift))
 		{
 			if (ImGui::BeginPopupContextItem("Menu"))
 			{
@@ -78,7 +94,7 @@ void UIRenderForm::Draw()
 				ImGui::EndPopup();
 			}
 		}
-		
+
 		draw_list->AddImage(UI->RT->pSurface, canvas_pos, ImVec2(canvas_pos.x + canvas_size.x, canvas_pos.y + canvas_size.y));
 
 	}
